028183d676eeda018210a530fb2872b6a579307a
Ruben Feurer <7510803+rubenfeurer@users.noreply.github.com>
2024-10-24 18:24:05 +0200

    Update main.py
    corrected sound files path

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from app import create_app
 from sounds import SoundManager
 
 volume = 50
-sound_folder = "/home/pi/pi_radio/sounds"
+sound_folder = "/home/radio/internetRadio/sounds"
 stream_manager = None
 sound_manager = None
 
