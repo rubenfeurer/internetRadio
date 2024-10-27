00792c77f4a62a68fd05bde2a54f8567b20afdb9
Ruben Feurer <7510803+rubenfeurer@users.noreply.github.com>
2024-10-27 15:29:25 +0100

    Update update_repo.sh
    added to log time

--- local_files/update_repo.sh
+++ local_files/update_repo.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# cd /home/radio/internetRadio
+# git pull origin main
+
+# git fetch origin
+# git reset --hard origin/main  # Replace 'main' with your branch name
+
 cd /home/radio/internetRadio
-git pull origin main
+
+GIT_OUTPUT=$(git pull 2>&1)
+echo "$(date '+%Y-%m-%d %H:%M:%S'): $GIT_OUTPUT" >> /home/radio/internetRadio/update_repo.log
