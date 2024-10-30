22aa380c7abff6ea0578ea274371c00c84649713
AmnaNaeem91 <amna091991@gmail.com>
2024-10-30 21:27:33 +0500

    Enabled Rotary encoder button during AP mode

--- main.py
+++ main.py
@@ -75,8 +75,14 @@ if __name__ == "__main__":
     sound_manager.play_sound("boot.wav")
 
     LED_PIN = 24
+    ENCODER_BUTTON = 23 # GPIO pin for Encoder Button
+
     led = LED(LED_PIN)
     led.on()
+    
+    buttonEn = Button(ENCODER_BUTTON, pull_up=True, bounce_time=0.2, hold_time=2)
+    buttonEn.when_pressed = lambda: print("Encoder Pressed")
+    buttonEn.when_held = lambda: restart_pi()
     
     if not check_wifi():
         print("Starting Wi-Fi hotspot...")
@@ -98,18 +104,14 @@ if __name__ == "__main__":
     BUTTON1_PIN = 17  # GPIO pin for Button 1
     BUTTON2_PIN = 27  # GPIO pin for Button 2
     BUTTON3_PIN = 22  # GPIO pin for Button 3
-    ENCODER_BUTTON = 23 # GPIO pin for Encoder Button
 
     button1 = Button(BUTTON1_PIN, pull_up=True, bounce_time=0.2)
     button2 = Button(BUTTON2_PIN, pull_up=True, bounce_time=0.2)
     button3 = Button(BUTTON3_PIN, pull_up=True, bounce_time=0.2)
-    buttonEn = Button(ENCODER_BUTTON, pull_up=True, bounce_time=0.2, hold_time=2)
 
     button1.when_pressed = lambda: button_handler('link1')
     button2.when_pressed = lambda: button_handler('link2')
     button3.when_pressed = lambda: button_handler('link3')
-    buttonEn.when_pressed = lambda: print("Encoder Pressed")
-    buttonEn.when_held = lambda: restart_pi()
 
     DT_PIN = 5  # GPIO pin for DT
     CLK_PIN = 6  # GPIO pin for CLK
